abd646873dda119d93b17589571c859c2d290425
hyeonginju <guddls64@gmail.com>
2021-04-12 11:35:44 +0900

    hh

--- App.jsx
+++ App.jsx
@@ -1,21 +1,17 @@
-import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, LogBox } from 'react-native';
+
+import { NavigationContainer } from '@react-navigation/native';
+import StackNavigator from './navigations/StackNavigator';
 
 export default function App() {
+  LogBox.ignoreLogs(['Warning: ...']);
+
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <NavigationContainer>
+      <StackNavigator />
+    </NavigationContainer>
   );
 }
 
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-});
+const styles = StyleSheet.create({});
---
+++ navigations/TabNavigator.jsx
@@ -0,0 +1,56 @@
+import React, { useEffect } from 'react';
+import { Platform } from 'react-native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
+import { Ionicons } from '@expo/vector-icons';
+
+import HomePage from '../pages/HomePage';
+import DoctorPage from '../pages/DoctorPage';
+
+const Tabs = createBottomTabNavigator();
+
+const TabNavigator = () => {
+  // 왜넣는지?
+  // const TabNavigator = ({ navigation, route }) => {
+  return (
+    <Tabs.Navigator
+      screenOptions={({ route }) => ({
+        tabBarIcon: ({ focused }) => {
+          let iconName = Platform.OS === 'ios' ? 'ios-' : 'md-';
+          if (route.name === 'Home') {
+            iconName += 'home-sharp';
+          } else if (route.name === 'Doctor') {
+            iconName += 'people';
+          }
+
+          return (
+            <Ionicons
+              name={iconName}
+              color={focused ? '#3AB27B' : '#AEAEAE'}
+              size={focused ? 28 : 26}
+            />
+          );
+        },
+      })}
+      tabBarOptions={{
+        showLabel: true,
+        labelStyle: {
+          fontSize: 11,
+          fontWeight: '500',
+          color: '#AEAEAE',
+          bottom: 4,
+        },
+        activeTintColor: '#3AB27B',
+        style: {
+          backgroundColor: '#fff',
+          borderTopColor: '#eee',
+          height: Platform.OS === 'ios' ? 85 : 60,
+        },
+      }}
+    >
+      <Tabs.Screen name="Home" component={HomePage} />
+      <Tabs.Screen name="Doctor" component={DoctorPage} />
+    </Tabs.Navigator>
+  );
+};
+
+export default TabNavigator;
